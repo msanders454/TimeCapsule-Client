bd6841d5c70f7b831f9898bddba445492d7d766f
Michael Sanders <mikesanderspsu@gmail.com>
2020-04-10 16:53:41 -0400

    added api url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: ' https://git.heroku.com/serene-ridge-50508.git',
+    API_ENDPOINT: 'https://serene-ridge-50508.herokuapp.com/api/expenses',
     TOKEN_KEY: 'API_TOKEN',
 }
